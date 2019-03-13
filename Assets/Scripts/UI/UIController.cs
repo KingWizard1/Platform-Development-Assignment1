0f3d2fab3a01aabdd45b96f7f7581375b0c0bc1f
perfectdark420 <47585882+perfectdark420@users.noreply.github.com>
2019-03-13 15:58:55 +1100

    Re-worked the UI and started adding scripts

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    private UILoginPanel _LoginPanel;
+
+    private UISignUpPanel _SignUpPanel;
+
+    private UIPasswordResetPanel1 _PasswordResetPanel1;
+
+    private UIPasswordResetPanel2 _PasswordResetPanel2;
+
+
+    // ----------------------------------------------------- //
+
+
+    // ----------------------------------------------------- //
+
+
+    // ----------------------------------------------------- //
+
+
+    // ----------------------------------------------------- //
+
+}
---
+++ Assets/Scripts/UI/UILoginPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UILoginPanel : MonoBehaviour {
+
+    public InputField usernameField;
+    public InputField passwordField;
+
+    public Button playButton;
+
+    public TextMeshProUGUI signupLink;
+    public TextMeshProUGUI forgotPasswordLink;
+
+    // ----------------------------------------------------- //
+
+
+    // ----------------------------------------------------- //
+
+
+    // ----------------------------------------------------- //
+
+
+}
